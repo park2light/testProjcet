da53e5b10a7f678a9b614d7ab92a25c7c9e76e87
park <park2light@gmail.com>
2021-05-14 10:59:30 +0900

    처음 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+
+const mongoose = require('mongoose');
+mongoose.connect
+('mongodb+srv://psh:1234@boilerplate.idmdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
+}).then(()=>console.log('MongoDB Connected~')).catch(err =>console.log(err))
+
+
+app.get('/',(req,res)=>res.send('Hello world~'))
+
+app.listen(port,()=>console.log('testProject Start on port on '+port))
