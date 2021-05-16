45f97e196b0a8c5a8d143a05d420f6a6aa62bf9b
park <park2light@gmail.com>
2021-05-16 11:46:10 +0900

    데이터베이스에 저장될 비밀번호 암호화 bcrypt

--- model/User.js
+++ model/User.js
@@ -1,4 +1,8 @@
 const mongoose =require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+
 
 const userSchema = mongoose.Schema({
     name:{
@@ -27,6 +31,28 @@ const userSchema = mongoose.Schema({
     },
     tokenExp:{
         type:Number
+    }
+})
+
+userSchema.pre('save',function(next){
+
+    var user = this;
+
+    if(user.isModified('password')){
+
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password,salt,function(err,hash){
+
+                if(err) return next(err)
+
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
     }
 })
 
